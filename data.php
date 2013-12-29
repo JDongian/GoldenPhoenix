5e419d252f661606bf7edd991a9667714306b13c
Joshua <jdong42@gmail.com>
2013-12-29 00:31:25 -0600

    removed something dumb.

--- data.php
+++ data.php
@@ -1,4 +1,4 @@
 <?php
 header('Content-Type: application/json; charset=iso-8859-1');
-echo json_encode(json_decode(file_get_contents(sprintf("http://localhost:5000/%s/%d", $_GET['cat'], $_GET['index']))));
+echo file_get_contents(sprintf("http://localhost:5000/%s/%d", $_GET['cat'], $_GET['index']));
 ?>
